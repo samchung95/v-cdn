c1a9323ca7d6aef91a727c1dfe539e4dcbeb3c96
Samuel Chung <41113285+samchung95@users.noreply.github.com>
2024-02-06 07:01:46 +0800

    Update setup_script.sh

--- setup_script.sh
+++ setup_script.sh
@@ -1,12 +1,1 @@
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-sudo apt update
-sudo apt install docker-ce
-
-sudo usermod -aG docker ${USER}
-newgrp docker
-
 docker build docker.io/samuelchung/valence:base
